c8c86de3534415eb1f7c8ab0d8da7221dd909b54
wilfredo <109392939+wilfrevarca@users.noreply.github.com>
2022-07-23 11:40:44 -0500

    Add files via upload

---
+++ bucle while/bucle while/src/bucle/Main.java
@@ -0,0 +1,14 @@
+package bucle;
+
+    public class Main {
+        public static void main(String[] args) {
+            int numero = 0;
+
+            while (numero< 3){
+
+                System.out.println(numero);
+                numero = numero+1;
+
+            }
+        }
+    }
